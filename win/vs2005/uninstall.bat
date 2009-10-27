1fff88d16ed8b4fec2838baa1232110d158d2e50
Frank Hunleth <frank.hunleth@hillcrestlabs.com>
2009-10-27 11:50:26 -0400

    Modified Windows distributable to just be an installer.

---
+++ win/vs2005/uninstall.bat
@@ -0,0 +1,2 @@
+@echo off
+msiexec /x {4A2C6CB4-DE53-4303-9EB9-8874DA8670B2}
--- winbuild.bat
+++ winbuild.bat
@@ -1,24 +1,18 @@
 @echo off
 
 SET RELEASE_VERSION=0.4rc3
-SET WORKDIR=libfreespace-examples
-SET ZIPNAME=libfreespace-examples-%RELEASE_VERSION%-win32
+SET NAME=libfreespace-examples-%RELEASE_VERSION%.msi
 
 REM This must be run from the Visual Studio 2005 Command Prompt
+
+REM Clean up
+erase %NAME%
 devenv win\vs2005\examples.sln /clean release
+
+REM Build
 devenv win\vs2005\examples.sln /build release
 
-echo Cleanup any previous work directories
-rd /S /Q %WORKDIR%
-echo Creating work directory
-mkdir %WORKDIR%
-echo Copying build products
-copy win\vs2005\release\libfreespace.dll %WORKDIR%
-copy win\vs2005\release\*.exe %WORKDIR%
+echo Copying installer
+copy win\vs2005\release\Installer\libfreespace-examples-installer.msi %NAME%
 
-REM Create ZIP file using Info-ZIP
-REM http://www.info-zip.org/Zip.html
-echo Zipping - be sure to have zip.exe in your path or you will get an error below
-zip -r %ZIPNAME%.zip %WORKDIR%
-rd /S /Q %WORKDIR%
 echo Done.
